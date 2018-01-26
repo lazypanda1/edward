f7e0cdc469e98ac1cd3f29774d82bfd6a8c0ef4b
Dustin Tran <dustinviettran@gmail.com>
2018-01-25 23:46:42 -0800

    replace ed.dot with tensor.dot(..., [[1], [0]])

--- edward/__init__.py
+++ edward/__init__.py
@@ -28,7 +28,7 @@ from edward.inferences import (
 # from edward.inferences import MonteCarlo, HMC, MetropolisHastings, SGLD, SGHMC, Gibbs
 from edward.models import RandomVariable
 from edward.util import (
-    copy, dot,
+    copy,
     get_ancestors, get_blanket, get_children, get_control_variate_coef,
     get_descendants, get_parents, get_siblings, get_variables,
     is_independent, random_variables,
@@ -67,7 +67,6 @@ _allowed_symbols = [
     'Gibbs',
     'RandomVariable',
     'copy',
-    'dot',
     'get_ancestors',
     'get_blanket',
     'get_children',
